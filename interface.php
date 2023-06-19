1aee53ac4facf5e232f56ec2a756ccbbe418f31a
rodrigo-salinet <rodrigo.salinet@gmail.com>
2023-06-19 00:56:37 -0300

    🔧 ➕  first interface

---
+++ interface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @ano número
+ * @mes número
+ * @dia número
+ * @hora número
+ * @minuto número
+ * @segundo número
+ * @milesimo número
+ */
+
+$ano = date("Y");
+$mes = date("m");
+$dia = date("d");
+$hora = date ("H");
+$minuto = date("i");
+$segundo = date("s");
+$milesimo = hrtime(true);
+?>
