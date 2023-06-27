13bff7ed85491d1cd5b214588b45c7b6f118dd46
rodrigo-salinet <rodrigo.salinet@gmail.com>
2023-06-27 12:14:40 -0300

    ✨  add action novo item

--- adicionar_item.php
+++ adicionar_item.php
@@ -1,37 +1,54 @@
 <?php
+// phpinfo(); die();
 session_start();
 require_once('conexao.php');
 
-$data['success'] = 1;
-$data['message'] = 'Ops! Nada ainda aconteceu.';
-$data['errors'] = null;
+$redirect = 'novo_item.php';
+$msg = "Nada aconteceu";
 
 if (!isset($_SESSION['logado'])) {
-    $data['success'] = 0;
-    $data['errors'] = "Usuário não logado.";
-} else if (isset($_POST['hdn_id_orcamento']) && isset($_POST['hdn_id_usuario']) && isset($_POST['hdn_id_item']) && isset($_POST['hdn_quantidade'])) {
-    $hdn_id_orcamento = $_POST['hdn_id_orcamento'];
-    $hdn_id_usuario = $_POST['hdn_id_usuario'];
-    $hdn_id_item = $_POST['hdn_id_item'];
-    $hdn_quantidade = $_POST['hdn_quantidade'];
+    $redirect = 'login.php';
+    $msg = "É necessário realizar o login para utilizar o sistema.";
+} else if (isset($_POST['txt_nome_item']) && isset($_POST['txt_descricao_item'])) {
+    $fil_upload_imagem = $_FILES['fil_upload_imagem']['name'];
+    $txt_nome_item = $_POST['txt_nome_item'];
+    $txt_descricao_item = $_POST['txt_descricao_item'];
 
     try {
-        $str_sql_itens_orcamentos = "select * from `tbl_itens_orcamentos` where `id_item` = $hdn_id_item and `id_orcamento` = $hdn_id_orcamento;";
-        $sql_itens_orcamentos = mysqli_query($conexao, $str_sql_itens_orcamentos);
-        $qtd_itens_orcamentos = mysqli_num_rows($sql_itens_orcamentos);
+        $diretorio_imagens = "./imagens/";
+        $arquivo_upload = basename($fil_upload_imagem);
+        $prefixo_nome_arquivo_upload = explode(".", $arquivo_upload)[0];
+        $extensao_arquivo_upload = strtolower(pathinfo($arquivo_upload,PATHINFO_EXTENSION));
+        $arquivo_destino = $diretorio_imagens . $prefixo_nome_arquivo_upload . '-' . md5(rand(1, 999999)) . '.' . $extensao_arquivo_upload;
+        $nome_arquivo_destino = basename($arquivo_destino);
 
-        if ($qtd_itens_orcamentos > 0) {
-            $data['message'] = '<i class="fa fa-ban text-danger"></i> Este item já foi adicionado! Escolha outro.';
-        } else {
-            $str_sql_adicionar_item = "insert into `tbl_itens_orcamentos` (`id_usuario`,`id_orcamento`,`id_item`,`quantidade`) values ($hdn_id_usuario, $hdn_id_orcamento, $hdn_id_item, $hdn_quantidade);";
-            $sql_adicionar_item = mysqli_query($conexao, $str_sql_adicionar_item);
-            $data['message'] = '<i class="fa fa-check-circle-o text-success"></i> Item adicionado com sucesso!';
+        if(!getimagesize($_FILES["fil_upload_imagem"]["tmp_name"])) {
+            throw new Exception("Ops! O arquivo enviado não é uma imagem válida. Por favor volte e selecione outro arquivo de imagem válida.");
         }
+
+        if (file_exists($arquivo_destino)) {
+            throw new Exception("Ops! Já existe um arquivo com o mesmo nome. Volte e tente novamente.");
+        }
+
+        if ($_FILES["fil_upload_imagem"]["size"] > 500000) {
+            throw new Exception("Ops! O arquivo enviado é muito grande. Volte e escolha outro arquivo menor que 50mb.");
+        }
+
+        if($extensao_arquivo_upload != "jpg" && $extensao_arquivo_upload != "png" && $extensao_arquivo_upload != "jpeg" && $extensao_arquivo_upload != "gif" ) {
+            throw new Exception("Ops! Somente arquivos com a extensão JPG, JPEG, PNG e GIF são aceitos.");
+        }
+
+        if (!move_uploaded_file($_FILES["fil_upload_imagem"]["tmp_name"], $arquivo_destino)) {
+            throw new Exception("Ops! Não foi possível enviar o arquivo. Volte e tente novamente.");
+        }
+
+        $str_sql_adicionar_item = "insert into `tbl_itens` (`imagem`,`nome`,`descricao`) values ('$nome_arquivo_destino', '$txt_nome_item', '$txt_descricao_item');";
+        $sql_adicionar_item = mysqli_query($conexao, $str_sql_adicionar_item);
+        $msg = 'Item adicionado com sucesso!';
     } catch (\Exception $e) {
-        $data['message'] = 'Não foi possível adicionar o item!';
-        $data['errors'] = "$e";
+        $msg = "Ocorreu o erro: ." . str_replace(array("\r", "\n"), '', $e);
     }
 }
 
-echo json_encode($data);
+header('Location: ' . $redirect . '?msg=' . htmlspecialchars($msg));
 ?>
