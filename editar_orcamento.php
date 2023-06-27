0d7c6256391bbcb0e5865f4c25b45a8d8ff94a87
rodrigo-salinet <rodrigo.salinet@gmail.com>
2023-06-27 12:23:11 -0300

    ✨ 💄  add quantidade ui

--- editar_orcamento.php
+++ editar_orcamento.php
@@ -20,25 +20,28 @@ $id_usuario = $_SESSION['id_usuario'];
 <body id="page-top" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="57">
     <?php require_once('navbar.php'); ?>
 
-    <section class="content mt-4">
-        <div class="container-fluid" id="sec_editar_orcamento">
+    <section class="content" id="sec_editar_orcamento">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col mb-3 text-center">
                     <h5 class="h5 text-center">
                         <i class="fa fa-money text-warning"></i>
-                        Editar Orçamento
+                        Editar/Excluir Orçamento
                     </h5>
                 </div>
             </div>
             <div class="row mb-3">
                 <div class="col text-center">
-                    ID Orçamento
+                    ID
                 </div>
                 <div class="col text-center">
                     Cliente
                 </div>
                 <div class="col text-center">
                     Itens
+                </div>
+                <div class="col text-center">
+                    Quantidades
                 </div>
                 <div class="col text-center">
                     Aprovado
@@ -66,7 +69,7 @@ $id_usuario = $_SESSION['id_usuario'];
                         <input type="text" class="form-control" id="txt_editar_id_orcamento<?php echo $id_orcamento; ?>" value="<?php echo $id_orcamento; ?>" disabled />
                         <a href="#" onclick="excluirOrcamento(this)" class="input-group-text" data-orcamento="<?php echo $id_orcamento; ?>"><i class="text-danger fa fa-trash" title="Excluir Orçamento"></i></a>
                     </div>
-                    <div class="container collapse" id="div_sucesso_editar_orcamento_id_orcamento<?php echo $id_item; ?>"></div>
+                    <div class="container collapse" id="div_sucesso_editar_orcamento_id_orcamento<?php echo $id_orcamento; ?>"></div>
                 </div>
                 <div class="col text-center">
                     <select class="form-select" id="sel_editar_id_cliente_orcamento<?php echo $id_orcamento; ?>" data-orcamento="<?php echo $id_orcamento; ?>" onchange="editarIdClienteOrcamento(this)">
@@ -101,9 +104,9 @@ $id_usuario = $_SESSION['id_usuario'];
 
                         for ($c = 0; $c < $qtd_itens_orcamentos; $c++) {
                             $itens_orcamentos = mysqli_fetch_array($sql_itens_orcamentos);
-                            $id_item = $itens_orcamentos['id'];
+                            $id_item_orcamento = $itens_orcamentos['id_item'];
 
-                            $str_sql_item = "select * from `tbl_itens` where `id` = $id_item;";
+                            $str_sql_item = "select * from `tbl_itens` where `id` = $id_item_orcamento;";
                             $sql_item = mysqli_query($conexao, $str_sql_item);
                             $qtd_item = mysqli_num_rows($sql_item);
 
@@ -112,11 +115,31 @@ $id_usuario = $_SESSION['id_usuario'];
                                 $nome_item = $item['nome'];
                             }
                     ?>
-                    <div class="input-group">
-                        <input type="text" class="form-control" id="txt_editar_orcamento_id_item" value="<?php echo $nome_item; ?>" disable>
-                        <a href="#" onclick="excluirItemOrcamento(this)" class="input-group-text" data-item="<?php echo $id_item; ?>" data-orcamento="<?php echo $id_orcamento; ?>"><i class="text-danger fa fa-trash" title="Excluir este Item"></i></a>
+                    <div class="input-group" id="ig_editar_orcamento<?php echo $id_orcamento; ?>_nome_item<?php echo $id_item_orcamento; ?>">
+                        <input type="text" class="form-control" id="txt_editar_orcamento<?php echo $id_orcamento; ?>_id_item<?php echo $id_item_orcamento; ?>" value="<?php echo $nome_item; ?>" title="[ID <?php echo $id_item_orcamento; ?>]" disabled>
+                        <a href="#" onclick="excluirItemOrcamento(this)" class="input-group-text" data-item="<?php echo $id_item_orcamento; ?>" data-orcamento="<?php echo $id_orcamento; ?>"><i class="text-danger fa fa-trash" title="Excluir este Item"></i></a>
                     </div>
-                    <div class="container collapse" id="div_sucesso_editar_orcamento_id_item<?php echo $id_item; ?>"></div>
+                    <div class="container collapse" id="div_sucesso_editar_orcamento<?php echo $id_orcamento; ?>_id_item<?php echo $id_item_orcamento; ?>"></div>
+                    <?php
+                        }
+                    ?>
+                </div>
+                <div class="col text-center">
+                    <?php
+                        $str_sql_itens_orcamentos = "select * from `tbl_itens_orcamentos` where `id_orcamento` = $id_orcamento;";
+                        $sql_itens_orcamentos = mysqli_query($conexao, $str_sql_itens_orcamentos);
+                        $qtd_itens_orcamentos = mysqli_num_rows($sql_itens_orcamentos);
+
+                        for ($c = 0; $c < $qtd_itens_orcamentos; $c++) {
+                            $itens_orcamentos = mysqli_fetch_array($sql_itens_orcamentos);
+                            $id_item_orcamento = $itens_orcamentos['id_item'];
+                            $quantidade_item = $itens_orcamentos['quantidade'];
+                    ?>
+                    <div class="input-group" id="ig_editar_orcamento<?php echo $id_orcamento; ?>_quantidade_item<?php echo $id_item_orcamento; ?>">
+                        <input type="text" class="form-control" id="txt_editar_orcamento<?php echo $id_orcamento; ?>_quantidade_item<?php echo $id_item_orcamento; ?>" value="<?php echo $quantidade_item; ?>">
+                        <a href="#" onclick="quantidadeItemOrcamento(this)" class="input-group-text" data-item="<?php echo $id_item_orcamento; ?>" data-orcamento="<?php echo $id_orcamento; ?>"><i class="text-warning fa fa-eraser" title="Alterar quantidade deste Item"></i></a>
+                    </div>
+                    <div class="container collapse" id="div_sucesso_editar_orcamento<?php echo $id_orcamento; ?>_quantidade_item<?php echo $id_item_orcamento; ?>"></div>
                     <?php
                         }
                     ?>
@@ -126,6 +149,11 @@ $id_usuario = $_SESSION['id_usuario'];
                 </div>
             </div>
             <?php } ?>
+            <form id="frm_editar_orcamento_quantidade" name="frm_editar_orcamento_quantidade">
+                <input type="hidden" name="hdn_editar_orcamento_quantidade_item_id_orcamento" id="hdn_editar_orcamento_quantidade_item_id_orcamento" />
+                <input type="hidden" name="hdn_editar_orcamento_quantidade_item_id_item" id="hdn_editar_orcamento_quantidade_item_id_item" />
+                <input type="hidden" name="hdn_editar_orcamento_quantidade_item" id="hdn_editar_orcamento_quantidade_item" />
+            </form>
             <form method="post" id="frm_editar_orcamento_id_cliente" name="frm_editar_orcamento_id_cliente">
                 <input type="hidden" name="hdn_editar_orcamento_editar_cliente_id_orcamento" id="hdn_editar_orcamento_editar_cliente_id_orcamento" />
                 <input type="hidden" name="hdn_editar_orcamento_editar_cliente_id_cliente" id="hdn_editar_orcamento_editar_cliente_id_cliente" />
