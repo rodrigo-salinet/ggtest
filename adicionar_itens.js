035865be676cbee8cb3e426ef0284c55a43cbaff
rodrigo-salinet <rodrigo.salinet@gmail.com>
2023-06-27 12:15:49 -0300

    ✨  add function atualizar quantidade

--- adicionar_itens.js
+++ adicionar_itens.js
@@ -4,13 +4,11 @@ function adicionarOrcamento(obj) {
     let txt_quantidade = document.getElementById('txt_quantidade' + id_item);
     let sel_orcamentos = document.getElementById('sel_orcamentos');
 
-    let frm_add_item = document.getElementById('frm_add_item');
     let hdn_id_orcamento = document.getElementById('hdn_id_orcamento');
     let hdn_id_usuario = document.getElementById('hdn_id_usuario');
     let hdn_id_item = document.getElementById('hdn_id_item');
     let hdn_quantidade = document.getElementById('hdn_quantidade');
 
-    // return alert(sel_orcamentos.value);
     if (sel_orcamentos.value == '0') {
         alert("Por favor, selecione um orçamento antes de prosseguir");
         return sel_orcamentos.focus();
@@ -31,10 +29,11 @@ function selecionarOrcamento(obj) {
 function diminuirQuantidade(obj) {
     let txt_quantidade = document.getElementById('txt_quantidade' + obj.dataset.item);
     let quantidade = parseInt(txt_quantidade.value);
-    if (quantidade > 1) {
+    if (quantidade > 0) {
         quantidade -= 1;
+        txt_quantidade.value = quantidade;
+        atualizarQuantidade(txt_quantidade);
     }
-    txt_quantidade.value = quantidade;
 }
 
 function aumentarQuantidade(obj) {
@@ -44,9 +43,34 @@ function aumentarQuantidade(obj) {
         quantidade += 1;
     }
     txt_quantidade.value = quantidade;
+    atualizarQuantidade(txt_quantidade);
+}
+
+function atualizarQuantidade(obj) {
+    let id_usuario = obj.dataset.user;
+    let id_item = obj.dataset.item;
+    let sel_orcamentos = document.getElementById('sel_orcamentos');
+
+    let hdn_atualizar_quantidade_id_orcamento = document.getElementById('hdn_atualizar_quantidade_id_orcamento');
+    let hdn_atualizar_quantidade_id_usuario = document.getElementById('hdn_atualizar_quantidade_id_usuario');
+    let hdn_atualizar_quantidade_id_item = document.getElementById('hdn_atualizar_quantidade_id_item');
+    let hdn_atualizar_quantidade_txt_quantidade = document.getElementById('hdn_atualizar_quantidade_txt_quantidade');
+
+    if (sel_orcamentos.value == '0') {
+        alert("Por favor, selecione um orçamento antes de prosseguir");
+        return sel_orcamentos.focus();
+    }
+
+    hdn_atualizar_quantidade_id_orcamento.value = sel_orcamentos.value;
+    hdn_atualizar_quantidade_id_usuario.value = id_usuario;
+    hdn_atualizar_quantidade_id_item.value = id_item;
+    hdn_atualizar_quantidade_txt_quantidade.value = obj.value;
+
+    $('#frm_atualizar_quantidade').trigger("submit");    
 }
 
 $(document).ready(function() {
+
     $('#frm_add_item').submit(function(e) {
         e.preventDefault();
         let hdn_id_item = document.getElementById('hdn_id_item');
@@ -66,4 +90,25 @@ $(document).ready(function() {
             }
         });
     });
+
+    $('#frm_atualizar_quantidade').submit(function(e) {
+        e.preventDefault();
+        let hdn_atualizar_quantidade_id_item = document.getElementById('hdn_atualizar_quantidade_id_item');
+        let div_sucesso = document.getElementById('div_sucesso' + hdn_atualizar_quantidade_id_item.value);
+        $.ajax({
+            type: "POST",
+            url: 'atualizar_item.php',
+            data: $(this).serialize(),
+            success: function(response) {
+                let jsonData = JSON.parse(response);
+                if (jsonData.success == "1") {
+                    div_sucesso.innerHTML = jsonData.message;
+                } else {
+                    div_sucesso.innerHTML = jsonData.error;
+                }
+                div_sucesso.classList.add("show");
+            }
+        });
+    });
+
 });
