f1a55c42fefb051fdb7bc495eb121a62d2e22169
rodrigo-salinet <rodrigo.salinet@gmail.com>
2023-06-28 15:08:50 -0300

    🚸 melhorias de ui aprovar orcamento

--- aprovar_orcamentos.php
+++ aprovar_orcamentos.php
@@ -40,31 +40,6 @@ $id_usuario = $_SESSION['id_usuario'];
                 </div>
             </div>
             <?php } else { ?>
-            <!-- <div class="row border-bottom border-5 border-dark mb-3 item3">
-                <div class="col text-center">
-                    &nbsp;
-                </div>
-            </div> -->
-            <div class="row mb-3">
-                <div class="col text-center">
-                    ID
-                </div>
-                <div class="col text-center">
-                    Cliente
-                </div>
-                <div class="col text-center">
-                    Itens
-                </div>
-                <div class="col text-center">
-                    Quantidades
-                </div>
-                <div class="col text-center">
-                    Preços
-                </div>
-                <div class="col text-center">
-                    Status
-                </div>
-            </div>
             <?php
                 $id_usuario_orcamento = $_GET['id_usuario'];
                 $str_sql_orcamentos = "select * from `tbl_orcamentos` where `id_usuario` = $id_usuario_orcamento;";
@@ -77,47 +52,38 @@ $id_usuario = $_SESSION['id_usuario'];
                     $id_cliente_orcamento = $orcamentos['id_cliente'];
                     $id_status_orcamento = $orcamentos['id_status_orcamento'];
             ?>
-            <div class="row" id="div_row_orcamento<?php echo $id_orcamento; ?>">
-                <div class="col text-center">
-                    <input type="text" class="form-control" id="txt_editar_id_orcamento<?php echo $id_orcamento; ?>" value="<?php echo $id_orcamento; ?>" disabled />
+            <div class="row border-top border-5 border-dark mb-3 item3">
+                <div class="col mt-3 text-center">
+                    Orçamento
                 </div>
+            </div>
+            <div class="row mb-3" id="div_row_id_orcamento<?php echo $id_orcamento; ?>">
                 <div class="col text-center">
-                        <?php
-                            $str_sql_clientes = "select * from `tbl_clientes` where `id` = $id_cliente_orcamento;";
-                            $sql_clientes = mysqli_query($conexao, $str_sql_clientes);
-                            $qtd_clientes = mysqli_num_rows($sql_clientes);
-
-                            for ($c = 0; $c < $qtd_clientes; $c++) {
-                                $clientes = mysqli_fetch_array($sql_clientes);
-                                $nome_cliente = $clientes['nome'];
-                            }
-                        ?>
-                    <input type="text" class="form-control" id="txt_editar_orcamento<?php echo $id_orcamento; ?>_cliente<?php echo $id_cliente_orcamento; ?>" value="<?php echo $nome_cliente; ?>" disabled />
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="txt_editar_id_orcamento<?php echo $id_orcamento; ?>" value="<?php echo $id_orcamento; ?>" disabled />
+                        <label for="txt_editar_id_orcamento<?php echo $id_orcamento; ?>">ID</label>
+                    </div>
                 </div>
+            </div>
+            <div class="row mb-3" id="div_row_nome_orcamento<?php echo $id_orcamento; ?>">
                 <div class="col text-center">
                     <?php
-                        $str_sql_itens_orcamentos = "select * from `tbl_itens_orcamentos` where `id_orcamento` = $id_orcamento;";
-                        $sql_itens_orcamentos = mysqli_query($conexao, $str_sql_itens_orcamentos);
-                        $qtd_itens_orcamentos = mysqli_num_rows($sql_itens_orcamentos);
+                        $str_sql_clientes = "select * from `tbl_clientes` where `id` = $id_cliente_orcamento;";
+                        $sql_clientes = mysqli_query($conexao, $str_sql_clientes);
+                        $qtd_clientes = mysqli_num_rows($sql_clientes);
 
-                        for ($c = 0; $c < $qtd_itens_orcamentos; $c++) {
-                            $itens_orcamentos = mysqli_fetch_array($sql_itens_orcamentos);
-                            $id_item_orcamento = $itens_orcamentos['id_item'];
-
-                            $str_sql_item = "select * from `tbl_itens` where `id` = $id_item_orcamento;";
-                            $sql_item = mysqli_query($conexao, $str_sql_item);
-                            $qtd_item = mysqli_num_rows($sql_item);
-
-                            for ($i = 0; $i < $qtd_item; $i++) {
-                                $item = mysqli_fetch_array($sql_item);
-                                $nome_item = $item['nome'];
-                            }
-                    ?>
-                    <input type="text" class="form-control" id="txt_editar_orcamento<?php echo $id_orcamento; ?>_id_item<?php echo $id_item_orcamento; ?>" value="<?php echo $nome_item; ?>" title="[ID <?php echo $id_item_orcamento; ?>]" disabled>
-                    <?php
+                        for ($c = 0; $c < $qtd_clientes; $c++) {
+                            $clientes = mysqli_fetch_array($sql_clientes);
+                            $nome_cliente = $clientes['nome'];
                         }
                     ?>
+                    <div class="form-floating">
+                        <input type="text" class="form-control" id="txt_editar_orcamento<?php echo $id_orcamento; ?>_cliente<?php echo $id_cliente_orcamento; ?>" value="<?php echo $nome_cliente; ?>" disabled />
+                        <label for="txt_editar_orcamento<?php echo $id_orcamento; ?>_cliente<?php echo $id_cliente_orcamento; ?>">Cliente</label>
+                    </div>
                 </div>
+            </div>
+            <div class="row mb-3" id="div_row_itens_orcamento<?php echo $id_orcamento; ?>">
                 <div class="col text-center">
                     <?php
                         $str_sql_itens_orcamentos = "select * from `tbl_itens_orcamentos` where `id_orcamento` = $id_orcamento;";
@@ -128,73 +94,99 @@ $id_usuario = $_SESSION['id_usuario'];
                             $itens_orcamentos = mysqli_fetch_array($sql_itens_orcamentos);
                             $id_item_orcamento = $itens_orcamentos['id_item'];
                             $quantidade_item = $itens_orcamentos['quantidade'];
-                    ?>
-                    <input type="text" class="form-control" id="txt_editar_orcamento<?php echo $id_orcamento; ?>_quantidade_item<?php echo $id_item_orcamento; ?>" value="<?php echo $quantidade_item; ?>" disabled>
-                    <?php
-                        }
-                    ?>
-                </div>
-                <!-- <div class="col text-center">
-                    <select class="form-select" id="precos_item<?php echo $id_item; ?>">
-                        <option selected disable>Clique aqui para selecionar o preço do fornecedor</option>
-                        <?php
-                            $str_sql_precos_item = "select * from `tbl_precos_itens` where `id_item` = '$id_item' order by `preco` asc;";
-                            $sql_precos_item = mysqli_query($conexao, $str_sql_precos_item);
-                            $qtd_precos_item = mysqli_num_rows($sql_precos_item);
-                            if ($qtd_precos_item > 3) { $qtd_precos_item = 3; }
-                            for ($p = 0; $p < $qtd_precos_item; $p++) {
-                                $precos_item = mysqli_fetch_array($sql_precos_item);
-                                $id_preco = $precos_item['id'];
-                                $id_fornecedor = $precos_item['id_fornecedor'];
-                                $preco_item = $precos_item['preco'];
-                                $str_sql_fornecedor = "select * from `tbl_fornecedores` where `id` = $id_fornecedor;";
-                                $sql_fornecedor = mysqli_query($conexao, $str_sql_fornecedor);
-                                $qtd_fornecedor = mysqli_num_rows($sql_fornecedor);
-                                for ($f = 0; $f < $qtd_fornecedor; $f++) {
-                                    $fornecedor = mysqli_fetch_array($sql_fornecedor);
-                                    $fornecedor_nome = $fornecedor['nome'];
-                                }
-                                $destaque = "";
-                                $txt_destaque = "";
-                                if ($p == 0) {
-                                    $destaque = 'class="fw-bold"';
-                                    $txt_destaque = " -> Melhor preço!";
-                                }
-                        ?>
-                        <option value="<?php echo $id_preco; ?>" <?php echo $destaque; ?>>R$<?php echo number_format($preco_item, 2, ',', '.'); ?> -> <?php echo $fornecedor_nome; ?><?php echo $txt_destaque; ?></option>
-                        <?php
-                            }
-                        ?>
-                    </select>
-                </div> -->
-                <div class="col text-center">
-                    <select class="form-select" data-orcamento="<?php echo $id_orcamento; ?>" id="sel_aprovar_orcamento_status<?php echo $id_orcamento; ?>" onchange="alterarStatus(this)" />
-                    <?php
-                        $str_sql_status_orcamento = "select * from `tbl_status_orcamento`;";
-                        $sql_status_orcamento = mysqli_query($conexao, $str_sql_status_orcamento);
-                        $qtd_status_orcamento = mysqli_num_rows($sql_status_orcamento);
 
-                        for ($s = 0; $s < $qtd_status_orcamento; $s++) {
-                            $status_orcamento = mysqli_fetch_array($sql_status_orcamento);
-                            $id_status = $status_orcamento['id'];
-                            $status = $status_orcamento['status'];
-                            $selecionado = '';
-                            if ($id_status == $id_status_orcamento) {
-                                $selecionado = 'selected';
+                            $str_sql_item = "select * from `tbl_itens` where `id` = $id_item_orcamento;";
+                            $sql_item = mysqli_query($conexao, $str_sql_item);
+                            $qtd_item = mysqli_num_rows($sql_item);
+
+                            for ($i = 0; $i < $qtd_item; $i++) {
+                                $item = mysqli_fetch_array($sql_item);
+                                $nome_item = $item['nome'];
                             }
                     ?>
-                        <option value="<?php echo $id_status; ?>" <?php echo $selecionado; ?>><?php echo $status;?></option>
+                    <div class="input-group">
+                        <div class="form-floating">
+                            <input type="text" class="form-control" id="txt_editar_orcamento<?php echo $id_orcamento; ?>_id_item<?php echo $id_item_orcamento; ?>" value="<?php echo $nome_item; ?>" title="[ID <?php echo $id_item_orcamento; ?>]" disabled>
+                            <label for="txt_editar_orcamento<?php echo $id_orcamento; ?>_id_item<?php echo $id_item_orcamento; ?>">Item</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="text" class="form-control" id="txt_editar_orcamento<?php echo $id_orcamento; ?>_quantidade_item<?php echo $id_item_orcamento; ?>" value="<?php echo $quantidade_item; ?>" disabled>
+                            <label for="txt_editar_orcamento<?php echo $id_orcamento; ?>_quantidade_item<?php echo $id_item_orcamento; ?>">Quantidade</label>
+                        </div>
+                        <div class="form-floating">
+                            <select class="form-select" id="sel_editar_orcamento<?php echo $id_orcamento; ?>_precos<?php echo $id_item; ?>" onchange="alterarPreco(this)" data-orcamento="<?php echo $id_orcamento; ?>" data-item="<?php echo $id_item; ?>">
+                                <option selected disable>Clique aqui para selecionar o preço do fornecedor</option>
+                                <?php
+                                    $str_sql_precos_item = "select * from `tbl_precos_itens` where `id_item` = '$id_item' order by `preco` asc;";
+                                    $sql_precos_item = mysqli_query($conexao, $str_sql_precos_item);
+                                    $qtd_precos_item = mysqli_num_rows($sql_precos_item);
+                                    if ($qtd_precos_item > 3) { $qtd_precos_item = 3; }
+                                    for ($p = 0; $p < $qtd_precos_item; $p++) {
+                                        $precos_item = mysqli_fetch_array($sql_precos_item);
+                                        $id_preco = $precos_item['id'];
+                                        $id_fornecedor = $precos_item['id_fornecedor'];
+                                        $preco_item = $precos_item['preco'];
+                                        $str_sql_fornecedor = "select * from `tbl_fornecedores` where `id` = $id_fornecedor;";
+                                        $sql_fornecedor = mysqli_query($conexao, $str_sql_fornecedor);
+                                        $qtd_fornecedor = mysqli_num_rows($sql_fornecedor);
+                                        for ($f = 0; $f < $qtd_fornecedor; $f++) {
+                                            $fornecedor = mysqli_fetch_array($sql_fornecedor);
+                                            $fornecedor_nome = $fornecedor['nome'];
+                                        }
+                                        $destaque = "";
+                                        $txt_destaque = "";
+                                        if ($p == 0) {
+                                            $destaque = 'class="fw-bold"';
+                                            $txt_destaque = " -> Melhor preço!";
+                                        }
+                                ?>
+                                <option value="<?php echo $id_preco; ?>" <?php echo $destaque; ?>>R$<?php echo number_format($preco_item, 2, ',', '.'); ?> -> <?php echo $fornecedor_nome; ?><?php echo $txt_destaque; ?></option>
+                                <?php
+                                    }
+                                ?>
+                            </select>
+                            <label for="sel_editar_orcamento<?php echo $id_orcamento; ?>_precos<?php echo $id_item; ?>">Preço</label>
+                        </div>
+                    </div>
                     <?php } ?>
-                    </select>
+                </div>
+            </div>
+            <div class="row mb-3" id="div_row_aprovar_orcamento<?php echo $id_orcamento; ?>">
+                <div class="col text-center">
+                    <div class="form-floating">
+                        <select class="form-select" data-orcamento="<?php echo $id_orcamento; ?>" id="sel_aprovar_orcamento_status<?php echo $id_orcamento; ?>" onchange="alterarStatus(this)" />
+                        <?php
+                            $str_sql_status_orcamento = "select * from `tbl_status_orcamento`;";
+                            $sql_status_orcamento = mysqli_query($conexao, $str_sql_status_orcamento);
+                            $qtd_status_orcamento = mysqli_num_rows($sql_status_orcamento);
+
+                            for ($s = 0; $s < $qtd_status_orcamento; $s++) {
+                                $status_orcamento = mysqli_fetch_array($sql_status_orcamento);
+                                $id_status = $status_orcamento['id'];
+                                $status = $status_orcamento['status'];
+                                $selecionado = '';
+                                if ($id_status == $id_status_orcamento) {
+                                    $selecionado = 'selected';
+                                }
+                        ?>
+                            <option value="<?php echo $id_status; ?>" <?php echo $selecionado; ?>><?php echo $status;?></option>
+                        <?php } ?>
+                        </select>
+                        <label for="sel_aprovar_orcamento_status<?php echo $id_orcamento; ?>">Status</label>
+                    </div>
                     <div class="container collapse" id="div_sucesso_aprovar_orcamentos_id_orcamento<?php echo $id_orcamento; ?>"></div>
                 </div>
             </div>
             <?php } ?>
+            <?php } ?>
+            <form id="frm_aprovar_orcamentos_preco" name="frm_aprovar_orcamentos_preco">
+                <input type="hidden" name="hdn_aprovar_orcamentos_preco_item_id_orcamento" id="hdn_aprovar_orcamentos_preco_item_id_orcamento" />
+                <input type="hidden" name="hdn_aprovar_orcamentos_preco_id_preco" id="hdn_aprovar_orcamentos_preco_id_preco" />
+            </form>
             <form id="frm_aprovar_orcamentos_status" name="frm_aprovar_orcamentos_status">
                 <input type="hidden" name="hdn_aprovar_orcamentos_status_item_id_orcamento" id="hdn_aprovar_orcamentos_status_item_id_orcamento" />
                 <input type="hidden" name="hdn_aprovar_orcamentos_status_id_status" id="hdn_aprovar_orcamentos_status_id_status" />
             </form>
-            <?php } ?>
         </div>
     </section>
 
