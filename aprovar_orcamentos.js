087f6272727e409d6ad1756ebc176158e621e510
rodrigo-salinet <rodrigo.salinet@gmail.com>
2023-06-29 14:27:56 -0300

    ✨ new js fn alterar preco aprovar orcamentos ui

--- aprovar_orcamentos.js
+++ aprovar_orcamentos.js
@@ -1,3 +1,19 @@
+function alterarPreco(obj) {
+    let id_orcamento = obj.dataset.orcamento;
+    let id_item = obj.dataset.item;
+    let id_preco = obj.value;
+
+    let hdn_aprovar_orcamentos_preco_item_id_orcamento = document.getElementById('hdn_aprovar_orcamentos_preco_item_id_orcamento');
+    let hdn_aprovar_orcamentos_preco_id_item = document.getElementById('hdn_aprovar_orcamentos_preco_id_item');
+    let hdn_aprovar_orcamentos_preco_id_preco = document.getElementById('hdn_aprovar_orcamentos_preco_id_preco');
+
+    hdn_aprovar_orcamentos_preco_item_id_orcamento.value = id_orcamento;
+    hdn_aprovar_orcamentos_preco_id_item.value = id_item;
+    hdn_aprovar_orcamentos_preco_id_preco.value = id_preco;
+
+    $('#frm_aprovar_orcamentos_preco').trigger("submit");
+}
+
 function alterarStatus(obj) {
     let id_orcamento = obj.dataset.orcamento;
     let id_status = obj.value;
@@ -37,72 +53,23 @@ $(document).ready(function() {
         });
     });
 
-    $('#frm_editar_orcamento_id_cliente').submit(function(e) {
+    $('#frm_aprovar_orcamentos_preco').submit(function(e) {
         e.preventDefault();
-        let hdn_editar_orcamento_editar_cliente_id_orcamento = document.getElementById('hdn_editar_orcamento_editar_cliente_id_orcamento');
-        let div_sucesso_editar_orcamento_id_cliente = document.getElementById('div_sucesso_editar_orcamento_id_cliente' + hdn_editar_orcamento_editar_cliente_id_orcamento.value);
+        let hdn_aprovar_orcamentos_preco_item_id_orcamento = document.getElementById('hdn_aprovar_orcamentos_preco_item_id_orcamento');
+        let hdn_aprovar_orcamentos_preco_id_item = document.getElementById('hdn_aprovar_orcamentos_preco_id_item');
+        let div_sucesso_aprovar_orcamento_id_item = document.getElementById('div_sucesso_aprovar_orcamento' + hdn_aprovar_orcamentos_preco_item_id_orcamento.value + '_id_item' + hdn_aprovar_orcamentos_preco_id_item.value);
         $.ajax({
             type: "POST",
-            url: 'editar_id_cliente_orcamento.php',
+            url: 'alterar_preco_orcamento.php',
             data: $(this).serialize(),
             success: function(response) {
                 let jsonData = JSON.parse(response);
                 if (jsonData.success == "1") {
-                    div_sucesso_editar_orcamento_id_cliente.innerHTML = jsonData.message;
+                    div_sucesso_aprovar_orcamento_id_item.innerHTML = jsonData.message;
                 } else {
-                    div_sucesso_editar_orcamento_id_cliente.innerHTML = jsonData.error;
+                    div_sucesso_aprovar_orcamento_id_item.innerHTML = jsonData.error;
                 }
-                div_sucesso_editar_orcamento_id_cliente.classList.add("show");
-            }
-        });
-    });
-
-    $('#frm_editar_orcamento_id_item').submit(function(e) {
-        e.preventDefault();
-        let hdn_editar_orcamento_excluir_item_id_orcamento = document.getElementById('hdn_editar_orcamento_excluir_item_id_orcamento');
-        let hdn_editar_orcamento_excluir_item_id_item = document.getElementById('hdn_editar_orcamento_excluir_item_id_item');
-        let ig_editar_orcamento_nome_item = document.getElementById('ig_editar_orcamento' + hdn_editar_orcamento_excluir_item_id_orcamento.value + '_nome_item' + hdn_editar_orcamento_excluir_item_id_item.value);
-        let ig_editar_orcamento_quantidade_item = document.getElementById('ig_editar_orcamento' + hdn_editar_orcamento_excluir_item_id_orcamento.value + '_quantidade_item' + hdn_editar_orcamento_excluir_item_id_item.value);
-        let div_sucesso_editar_orcamento_id_item = document.getElementById('div_sucesso_editar_orcamento' + hdn_editar_orcamento_excluir_item_id_orcamento.value + '_id_item' + hdn_editar_orcamento_excluir_item_id_item.value);
-        let div_sucesso_editar_orcamento_quantidade_item = document.getElementById('div_sucesso_editar_orcamento' + hdn_editar_orcamento_excluir_item_id_orcamento.value + '_quantidade_item' + hdn_editar_orcamento_excluir_item_id_item.value);
-        $.ajax({
-            type: "POST",
-            url: 'excluir_id_item_orcamento.php',
-            data: $(this).serialize(),
-            success: function(response) {
-                let jsonData = JSON.parse(response);
-                if (jsonData.success == "1") {
-                    div_sucesso_editar_orcamento_id_item.innerHTML = jsonData.message;
-                    div_sucesso_editar_orcamento_quantidade_item.innerHTML = jsonData.message;
-                } else {
-                    div_sucesso_editar_orcamento_id_item.innerHTML = jsonData.error;
-                    div_sucesso_editar_orcamento_quantidade_item.innerHTML = jsonData.error;
-                }
-                ig_editar_orcamento_nome_item.innerHTML = '';
-                ig_editar_orcamento_quantidade_item.innerHTML = '';
-                div_sucesso_editar_orcamento_id_item.classList.add("show");
-                div_sucesso_editar_orcamento_quantidade_item.classList.add("show");
-            }
-        });
-    });
-
-    $('#frm_editar_orcamento_id_orcamento').submit(function(e) {
-        e.preventDefault();
-        let hdn_editar_orcamento_excluir_orcamento_id_orcamento = document.getElementById('hdn_editar_orcamento_excluir_orcamento_id_orcamento');
-        let div_sucesso_editar_orcamento_id_orcamento = document.getElementById('div_sucesso_editar_orcamento_id_orcamento' + hdn_editar_orcamento_excluir_orcamento_id_orcamento.value);
-        let div_row_orcamento = document.getElementById('div_row_orcamento' + hdn_editar_orcamento_excluir_orcamento_id_orcamento.value);
-        $.ajax({
-            type: "POST",
-            url: 'excluir_id_orcamento.php',
-            data: $(this).serialize(),
-            success: function(response) {
-                let jsonData = JSON.parse(response);
-                if (jsonData.success == "1") {
-                    div_row_orcamento.innerHTML = jsonData.message;
-                } else {
-                    div_sucesso_editar_orcamento_id_orcamento.innerHTML = jsonData.error;
-                    div_sucesso_editar_orcamento_id_orcamento.classList.add("show");
-                }
+                div_sucesso_aprovar_orcamento_id_item.classList.add("show");
             }
         });
     });
