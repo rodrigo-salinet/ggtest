dd938b535e885cb080331867408cd1e06304a0a5
rodrigo-salinet <rodrigo.salinet@gmail.com>
2023-06-29 11:40:40 -0300

    💄 chg margins novo item ui

--- novo_item.php
+++ novo_item.php
@@ -23,7 +23,7 @@ $id_usuario = $_SESSION['id_usuario'];
     <section class="content" id="sec_novo_item">
         <div class="container-fluid">
             <div class="row">
-                <div class="col mb-3 text-center">
+                <div class="col text-center">
                     <h5 class="h5 text-center">
                         <i class="fa fa-shopping-cart text-warning"></i>
                         Novo Item
@@ -31,44 +31,32 @@ $id_usuario = $_SESSION['id_usuario'];
                 </div>
             </div>
             <form action="adicionar_item.php" method="post" id="frm_novo_item" name="frm_novo_item" enctype="multipart/form-data">
-                <div class="row">
-                    <div class="col mb-3 text-center">
-                        <div class="input-group mb-3">
-                            <span class="input-group-text">Enviar Imagem</span>
+                <div class="row mb-3">
+                    <div class="col text-center">
+                        <div class="form-floating">
                             <input type="file" class="form-control" name="fil_upload_imagem" id="fil_upload_imagem" />
+                            <label for="fil_upload_imagem">Enviar Imagem</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col text-center">
+                        <div class="form-floating">
+                            <input type="text" class="form-control" name="txt_nome_item" id="txt_nome_item" />
+                            <label for="txt_nome_item">Nome</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col text-center">
+                        <div class="form-floating">
+                            <input type="text" class="form-control" name="txt_descricao_item" id="txt_descricao_item" />
+                            <label for="txt_descricao_item">Descrição</label>
                         </div>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col mb-3 text-center">
-                        <div class="input-group has-validation mb-3">
-                            <span class="input-group-text is-invalid">@</span>
-                            <div class="form-floating is-invalid">
-                                <input type="text" class="form-control is-invalid" name="txt_nome_item" id="txt_nome_item" placeholder="Digite aqui o nome do item" required />
-                                <label for="txt_nome_item">Nome</label>
-                            </div>
-                            <div class="invalid-feedback">
-                                Por favor, digite um nome acima para poder Criar Item.
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col mb-3 text-center">
-                        <div class="input-group has-validation mb-3">
-                            <span class="input-group-text">@</span>
-                            <div class="form-floating is-invalid">
-                                <input type="text" class="form-control is-invalid" name="txt_descricao_item" id="txt_descricao_item" placeholder="Digite aqui a descrição do item" required />
-                                <label for="txt_descricao_item">Descrição</label>
-                            </div>
-                            <div class="invalid-feedback">
-                                Por favor, digite uma descrição acima para poder Criar Item.
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col mb-3 text-center">
+                    <div class="col text-center">
                         <input type="submit" class="btn btn-outline-success" value="Criar item">
                     </div>
                 </div>
