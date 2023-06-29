d39d1d13cddcc20652c05c7f2afcb39cd722b1ae
rodrigo-salinet <rodrigo.salinet@gmail.com>
2023-06-29 11:38:48 -0300

    🐛 fix sem imagem adicionar itens

--- adicionar_item.php
+++ adicionar_item.php
@@ -10,36 +10,41 @@ if (!isset($_SESSION['logado'])) {
     $redirect = 'login.php';
     $msg = "É necessário realizar o login para utilizar o sistema.";
 } else if (isset($_POST['txt_nome_item']) && isset($_POST['txt_descricao_item'])) {
-    $fil_upload_imagem = $_FILES['fil_upload_imagem']['name'];
     $txt_nome_item = $_POST['txt_nome_item'];
     $txt_descricao_item = $_POST['txt_descricao_item'];
+    $nome_arquivo_destino = '';
 
     try {
-        $diretorio_imagens = "./imagens/";
-        $arquivo_upload = basename($fil_upload_imagem);
-        $prefixo_nome_arquivo_upload = explode(".", $arquivo_upload)[0];
-        $extensao_arquivo_upload = strtolower(pathinfo($arquivo_upload,PATHINFO_EXTENSION));
-        $arquivo_destino = $diretorio_imagens . $prefixo_nome_arquivo_upload . '-' . md5(rand(1, 999999)) . '.' . $extensao_arquivo_upload;
-        $nome_arquivo_destino = basename($arquivo_destino);
+        if (isset($_FILES['fil_upload_imagem'])) {
+            $fil_upload_imagem = $_FILES['fil_upload_imagem']['name'];
+            if($fil_upload_imagem != '') {
+                $diretorio_imagens = "./imagens/";
+                $arquivo_upload = basename($fil_upload_imagem);
+                $prefixo_nome_arquivo_upload = explode(".", $arquivo_upload)[0];
+                $extensao_arquivo_upload = strtolower(pathinfo($arquivo_upload,PATHINFO_EXTENSION));
+                $arquivo_destino = $diretorio_imagens . $prefixo_nome_arquivo_upload . '-' . md5(rand(1, 999999)) . '.' . $extensao_arquivo_upload;
+                $nome_arquivo_destino = basename($arquivo_destino);
 
-        if(!getimagesize($_FILES["fil_upload_imagem"]["tmp_name"])) {
-            throw new Exception("Ops! O arquivo enviado não é uma imagem válida. Por favor volte e selecione outro arquivo de imagem válida.");
-        }
+                if(!getimagesize($_FILES["fil_upload_imagem"]["tmp_name"])) {
+                    throw new Exception("Ops! O arquivo enviado não é uma imagem válida. Por favor volte e selecione outro arquivo de imagem válida.");
+                }
 
-        if (file_exists($arquivo_destino)) {
-            throw new Exception("Ops! Já existe um arquivo com o mesmo nome. Volte e tente novamente.");
-        }
+                if (file_exists($arquivo_destino)) {
+                    throw new Exception("Ops! Já existe um arquivo com o mesmo nome. Volte e tente novamente.");
+                }
 
-        if ($_FILES["fil_upload_imagem"]["size"] > 500000) {
-            throw new Exception("Ops! O arquivo enviado é muito grande. Volte e escolha outro arquivo menor que 50mb.");
-        }
+                if ($_FILES["fil_upload_imagem"]["size"] > 500000) {
+                    throw new Exception("Ops! O arquivo enviado é muito grande. Volte e escolha outro arquivo menor que 50mb.");
+                }
 
-        if($extensao_arquivo_upload != "jpg" && $extensao_arquivo_upload != "png" && $extensao_arquivo_upload != "jpeg" && $extensao_arquivo_upload != "gif" ) {
-            throw new Exception("Ops! Somente arquivos com a extensão JPG, JPEG, PNG e GIF são aceitos.");
-        }
+                if($extensao_arquivo_upload != "jpg" && $extensao_arquivo_upload != "png" && $extensao_arquivo_upload != "jpeg" && $extensao_arquivo_upload != "gif" ) {
+                    throw new Exception("Ops! Somente arquivos com a extensão JPG, JPEG, PNG e GIF são aceitos.");
+                }
 
-        if (!move_uploaded_file($_FILES["fil_upload_imagem"]["tmp_name"], $arquivo_destino)) {
-            throw new Exception("Ops! Não foi possível enviar o arquivo. Volte e tente novamente.");
+                if (!move_uploaded_file($_FILES["fil_upload_imagem"]["tmp_name"], $arquivo_destino)) {
+                    throw new Exception("Ops! Não foi possível enviar o arquivo. Volte e tente novamente.");
+                }
+            }
         }
 
         $str_sql_adicionar_item = "insert into `tbl_itens` (`imagem`,`nome`,`descricao`) values ('$nome_arquivo_destino', '$txt_nome_item', '$txt_descricao_item');";
