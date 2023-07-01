fc4416970d69d777e3aa07fd88dff2bebe7b3f9c
rodrigo-salinet <rodrigo.salinet@gmail.com>
2023-07-01 01:56:40 -0300

    ✨ add last id from insert mysql adicionar cliente

--- adicionar_cliente.php
+++ adicionar_cliente.php
@@ -4,6 +4,7 @@ require_once('conexao.php');
 
 $redirect = 'novo_cliente.php';
 $msg = "Nada aconteceu";
+$get_id_cliente = '';
 
 if (!isset($_SESSION['logado'])) {
     $redirect = 'login.php';
@@ -14,12 +15,22 @@ if (!isset($_SESSION['logado'])) {
 
     try {
         $str_sql_adicionar_cliente = "insert into `tbl_clientes` (`nome`) values ('$txt_nome_cliente');";
-        $sql_adicionar_cliente = mysqli_query($conexao, $str_sql_adicionar_cliente);
-        $msg = 'Cliente adicionado com sucesso!';
+        if ($conexao->query($str_sql_adicionar_cliente) === TRUE) {
+            $last_id = $conexao->insert_id;
+            $msg = "Cliente $txt_nome_cliente [ID $last_id] adicionado com sucesso!";
+
+            if (isset($_POST['chk_redirect'])) {
+                $chk_redirect = $_POST['chk_redirect'];
+                if ($chk_redirect == 'on') {
+                    $redirect = 'novo_orcamento.php';
+                    $get_id_cliente = '&id_cliente=' . htmlspecialchars($last_id);
+                }
+            }
+        }
     } catch (\Exception $e) {
         $msg = "Ocorreu o erro: ." . str_replace(array("\r", "\n"), '', $e);
     }
 }
 
-header('Location: ' . $redirect . '?msg=' . htmlspecialchars($msg));
+header('Location: ' . $redirect . '?msg=' . htmlspecialchars($msg) . $get_id_cliente);
 ?>
