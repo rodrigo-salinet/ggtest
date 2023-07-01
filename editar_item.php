5efa1ddab3a4fb29727ec6f74776af8da6388632
rodrigo-salinet <rodrigo.salinet@gmail.com>
2023-07-01 02:05:38 -0300

    💄 new ui alignments editar item divs sucesso

--- editar_item.php
+++ editar_item.php
@@ -22,7 +22,7 @@ $id_usuario = $_SESSION['id_usuario'];
 
     <section class="content" id="sec_editar_item">
         <div class="container-fluid">
-            <div class="row">
+            <div class="row align-items-center">
                 <div class="col text-center">
                     <h5 class="h5 text-center">
                         <i class="fa fa-shopping-cart text-warning"></i>
@@ -45,26 +45,24 @@ $id_usuario = $_SESSION['id_usuario'];
                 $nome_item = $item['nome'];
                     $descricao_item = $item['descricao'];
             ?>
-            <div class="row border-bottom border-5 border-dark mb-3 item<?php echo $id_item; ?>">
+            <div class="row align-items-center border-bottom border-5 border-dark mb-3 item<?php echo $id_item; ?>">
                 <div class="col text-center">
                     &nbsp;
                 </div>
             </div>
-            <div class="row mb-3 text-center item<?php echo $id_item; ?>" id="div_id_item<?php echo $id_item; ?>">
+            <div class="row align-items-center mb-3 text-center item<?php echo $id_item; ?>" id="div_id_item<?php echo $id_item; ?>">
                 <div class="col text-center">
                     <div class="input-group">
                         <div class="form-floating">
                             <input type="text" class="form-control" name="txt_editar_id_item<?php echo $id_item; ?>" id="txt_editar_id_item<?php echo $id_item; ?>" value="<?php echo $id_item; ?>" disabled />
                             <label for="txt_editar_id_item<?php echo $id_item; ?>">ID</label>
                         </div>
-                        <a href="#" onclick="excluirItem(this)" data-item="<?php echo $id_item; ?>" class="input-group-text" title="Excluir Item"><i class="fa fa-trash text-danger"></i></a>
+                        <a href="#stay" onclick="excluirItem(this)" data-item="<?php echo $id_item; ?>" class="input-group-text" title="Excluir Item"><i class="fa fa-trash text-danger"></i></a>
                     </div>
-                    <div class="container collapse" id="div_sucesso_editar_item_id_item<?php echo $id_item; ?>">
-                        .
-                    </div>
+                    <div class="container collapse" id="div_sucesso_editar_item_id_item<?php echo $id_item; ?>"></div>
                 </div>
             </div>
-            <div class="row mb-3 item<?php echo $id_item; ?>">
+            <div class="row align-items-center mb-3 item<?php echo $id_item; ?>">
                 <div class="col text-center">
                     <div class="input-group text-center mb-3 justify-content-center">
                         <img src="./imagens/<?php echo $imagem_item; ?>" loading="lazy" class="img-item" id="img_editar_item<?php echo $id_item; ?>">
@@ -82,31 +80,33 @@ $id_usuario = $_SESSION['id_usuario'];
                             <input type="file" class="form-control" id="fil_upload_imagem<?php echo $id_item; ?>" />
                             <label for="fil_upload_imagem<?php echo $id_item; ?>" class="bg-transparent">Editar Imagem</label>
                         </div>
-                        <a href="#" onclick="alterarImagemItem(this)" data-item="<?php echo $id_item; ?>" class="input-group-text" title="Alterar Imagem do Item"><i class="fa fa-eraser text-warning"></i></a>
+                        <a href="#stay" onclick="alterarImagemItem(this)" data-item="<?php echo $id_item; ?>" class="input-group-text" title="Alterar Imagem do Item"><i class="fa fa-eraser text-warning"></i></a>
                     </div>
                     <div class="container collapse" id="div_sucesso_editar_item_imagem<?php echo $id_item; ?>"></div>
                 </div>
             </div>
-            <div class="row mb-3 item<?php echo $id_item; ?>">
+            <div class="row align-items-center mb-3 item<?php echo $id_item; ?>">
                 <div class="col text-center">
                     <div class="input-group">
                         <div class="form-floating">
-                            <input type="text" class="form-control" name="txt_nome_item<?php echo $id_item; ?>" id="txt_nome_item<?php echo $id_item; ?>" placeholder="Digite aqui o nome do item" value="<?php echo $nome_item; ?>" />
+                            <input type="text" class="form-control" name="txt_nome_item<?php echo $id_item; ?>" id="txt_nome_item<?php echo $id_item; ?>" value="<?php echo $nome_item; ?>" />
                             <label for="txt_nome_item<?php echo $id_item; ?>">Nome</label>
                         </div>
-                        <a href="#" onclick="alterarNomeItem(this)" data-item="<?php echo $id_item; ?>" class="input-group-text" title="Alterar Nome do Item"><i class="fa fa-eraser text-warning"></i></a>
+                        <a href="#stay" onclick="alterarNomeItem(this)" data-item="<?php echo $id_item; ?>" class="input-group-text" title="Alterar Nome do Item"><i class="fa fa-eraser text-warning"></i></a>
                     </div>
+                    <div class="container collapse" id="div_sucesso_editar_item_nome<?php echo $id_item; ?>"></div>
                 </div>
             </div>
-            <div class="row mb-3 item<?php echo $id_item; ?>">
+            <div class="row align-items-center mb-3 item<?php echo $id_item; ?>">
                 <div class="col text-center">
                     <div class="input-group">
                         <div class="form-floating">
-                            <input type="text" class="form-control" name="txt_descricao_item<?php echo $id_item; ?>" id="txt_descricao_item<?php echo $id_item; ?>" placeholder="Digite aqui a descrição do item" value="<?php echo $descricao_item; ?>" />
+                            <input type="text" class="form-control" name="txt_descricao_item<?php echo $id_item; ?>" id="txt_descricao_item<?php echo $id_item; ?>" value="<?php echo $descricao_item; ?>" />
                             <label for="txt_descricao_item<?php echo $id_item; ?>">Descrição</label>
                         </div>
-                        <a href="#" onclick="alterarDescricaoItem(this)" data-item="<?php echo $id_item; ?>" class="input-group-text" title="Alterar Descrição do Item"><i class="fa fa-eraser text-warning"></i></a>
+                        <a href="#stay" onclick="alterarDescricaoItem(this)" data-item="<?php echo $id_item; ?>" class="input-group-text" title="Alterar Descrição do Item"><i class="fa fa-eraser text-warning"></i></a>
                     </div>
+                    <div class="container collapse" id="div_sucesso_editar_item_descricao<?php echo $id_item; ?>"></div>
                 </div>
             </div>
             <?php } ?>
