e487b77729d847fed5a4a593d8110483c7185112
rodrigo-salinet <rodrigo.salinet@gmail.com>
2023-07-01 01:58:28 -0300

    ✨ add last id from insert mysql & redirect add orc

--- adicionar_orcamento.php
+++ adicionar_orcamento.php
@@ -4,6 +4,7 @@ require_once('conexao.php');
 
 $redirect = 'novo_orcamento.php';
 $msg = "Nada aconteceu";
+$get_id_orcamento = '';
 
 if (!isset($_SESSION['logado'])) {
     $redirect = 'login.php';
@@ -14,12 +15,22 @@ if (!isset($_SESSION['logado'])) {
 
     try {
         $str_sql_adicionar_orcamento = "insert into `tbl_orcamentos` (`id_usuario`,`id_cliente`,`dia`,`mes`,`ano`,`hora`,`minuto`,`segundo`) values ($id_usuario, $sel_clientes, $dia, $mes, $ano, $hora, $minuto, $segundo);";
-        $sql_adicionar_item = mysqli_query($conexao, $str_sql_adicionar_orcamento);
-        $msg = 'Orçamento adicionado com sucesso!';
+        if ($conexao->query($str_sql_adicionar_orcamento) === TRUE) {
+            $last_id = $conexao->insert_id;
+            $msg = "Orçamento [ID $last_id] adicionado com sucesso!";
+
+            if (isset($_POST['chk_redirect'])) {
+                $chk_redirect = $_POST['chk_redirect'];
+                if ($chk_redirect == 'on') {
+                    $redirect = 'adicionar_itens.php';
+                    $get_id_orcamento = '&id_orcamento='. htmlspecialchars($last_id);
+                }
+            }
+        }
     } catch (\Exception $e) {
         $msg = "Ocorreu o erro: ." . str_replace(array("\r", "\n"), '', $e);
     }
 }
 
-header('Location: ' . $redirect . '?msg=' . htmlspecialchars($msg));
+header('Location: ' . $redirect . '?msg=' . htmlspecialchars($msg) . $get_id_orcamento);
 ?>
