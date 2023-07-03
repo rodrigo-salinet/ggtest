4eebfd1e3d162a888a5e1e71d816959d1887e1df
rodrigo-salinet <rodrigo.salinet@gmail.com>
2023-07-03 16:01:51 -0300

    🐛 fix $ before php var atualizar item

--- atualizar_item.php
+++ atualizar_item.php
@@ -35,7 +35,7 @@ if (!isset($_SESSION['logado'])) {
             $str_sql_adicionar_item_orcamento = "insert into `tbl_itens_orcamentos` (`id_orcamento`,`id_item`,`quantidade`) values ($hdn_atualizar_quantidade_id_orcamento, $hdn_atualizar_quantidade_id_item, $hdn_atualizar_quantidade_txt_quantidade);";
             if ($conexao->query($str_sql_adicionar_item_orcamento) === TRUE) {
                 $last_id = $conexao->insert_id;
-                $data['message'] = '<i class="fa fa-check-circle-o text-success"></i> Item [ID ' . hdn_atualizar_quantidade_id_item . '] adicionado com sucesso [ Novo ID vinculado ' . $last_id . ']!';
+                $data['message'] = '<i class="fa fa-check-circle-o text-success"></i> Item [ID ' . $hdn_atualizar_quantidade_id_item . '] adicionado com sucesso [Novo ID vinculado ' . $last_id . ']!';
             }
         }
     } catch (\Exception $e) {
