1442f246adecc03c4d920317a3caa6d75c027449
rodrigo-salinet <rodrigo.salinet@gmail.com>
2023-07-04 14:07:43 -0300

    🚸 add id item redirect ad preco item action

--- adicionar_preco_item.php
+++ adicionar_preco_item.php
@@ -5,6 +5,7 @@ require_once('conexao.php');
 
 $redirect = 'novo_preco_item.php';
 $msg = "Nada aconteceu";
+$id_item_get = '';
 
 if (!isset($_SESSION['logado'])) {
     $redirect = 'login.php';
@@ -15,6 +16,7 @@ if (!isset($_SESSION['logado'])) {
     $txt_preco_item = $_POST['txt_preco_item'];
     $preco_item = str_replace(".", "", $txt_preco_item);
     $preco_item = str_replace(",", ".", $preco_item);
+    $id_item_get = '&id_item=' . htmlspecialchars($_POST['sel_id_item']);
 
     try {
         $str_sql_adicionar_preco_item = "insert into `tbl_precos_itens` (`id_item`,`id_fornecedor`,`preco`) values ($sel_id_item, $sel_id_fornecedor, $preco_item);";
@@ -27,5 +29,5 @@ if (!isset($_SESSION['logado'])) {
     }
 }
 
-header('Location: ' . $redirect . '?msg=' . htmlspecialchars($msg));
+header('Location: ' . $redirect . '?msg=' . htmlspecialchars($msg) . $id_item_get);
 ?>
